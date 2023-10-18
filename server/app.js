7bf695ebefd11e56305f6ec84295c799d28ad2ed
sjlee80 <sjlee80@gmail.com>
2023-10-18 21:41:36 +0900

    Fix: cross domain

--- server/app.js
+++ server/app.js
@@ -17,8 +17,6 @@ app.use(
     name: 'cookie-session',
     keys: [process.env.SESSION_SECRET],
     expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 1 month
-    SameSite: 'none',
-    //secure: true,
   })
 );
 
