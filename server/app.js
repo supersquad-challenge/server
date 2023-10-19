68e979946e981db641e75033b9415542536ce9a4
sjlee80 <sjlee80@gmail.com>
2023-10-19 22:32:42 +0900

    Fix: redirect

--- server/app.js
+++ server/app.js
@@ -44,7 +44,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 //app.use('/static', express.static(path.join(__dirname, 'public')));
-//app.use(express.static('public'));
+app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
